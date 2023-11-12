bd77780a691139df52727c618d35d23120cf16f7
Ayşenur Kürklü <119884504+ayseck@users.noreply.github.com>
2023-11-12 18:59:36 +0300

    Create histogram_odevi.py
    Bir "ev" fotoğrafının grileştiren ve histogramını hesaplayan program.

---
+++ histogram_odevi.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+foto_gray=cv2.imread(r"ev.jfif",0)
+cv2.imshow("ev_gri",foto_gray)
+cv2.waitKey()
+
+# piksel değerleri ve frekanslarını saklamak için boş bir sözlük oluşturuyoruz
+histogram = {}
+
+# for döngüsü ile her piksel değeri tek tek inceleniyor
+for row in foto_gray:
+    for pixel in row:
+        if pixel not in histogram:
+            histogram[pixel] = 1
+        else:
+            histogram[pixel] += 1
+
+# Histogram verilerinin grafikte gösterimi
+plt.bar(histogram.keys(), histogram.values())
+plt.xlabel('Piksel Değeri')
+plt.ylabel('Frekans')
+plt.title('Görüntü Histogramı')
+plt.show()
